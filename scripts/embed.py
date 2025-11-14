1c85c2db83006e5661de23d4c2806ea6356d3e97
stepanyanhayk <haykstepanyan02@gmail.com>
2025-11-13 21:07:55 -0500

    add embedding script

---
+++ scripts/embed.py
@@ -0,0 +1,41 @@
+import argparse
+import time
+
+from medretrieval import Corpus, Embedding
+from datasets import load_dataset
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate embeddings for medical text documents")
+    parser.add_argument("--dataset_url", type=str, required=True, help="HF dataset name")
+    parser.add_argument("--dataset_name", type=str, required=True, help="HF dataset name")
+    parser.add_argument("--dataset_split", type=str, required=True, help="HF dataset split")
+    parser.add_argument("--model", type=str, required=True, help="HF model name")
+    parser.add_argument("--chunk_size", type=int, default=500, help="Maximum number of tokens per chunk (default: 500)")
+    parser.add_argument("--document_id_column", type=str, default="document_id", help="Column name to use as document ID (default: document_id)")
+    parser.add_argument("--content_column", type=str, default="content", help="Column name to use as content (default: content)")
+    parser.add_argument("--output_dir", type=str, default="experiments/embeddings/", help="Directory to save the dataset with embeddings")
+    args = parser.parse_args()
+
+    # Load dataset
+    print(f"Loading dataset from {args.dataset_url}/{args.dataset_name}/{args.dataset_split}")
+    dataset = load_dataset(args.dataset_url, args.dataset_name, split=args.dataset_split)
+    dataset = dataset.rename_column(args.document_id_column, "document_id")
+    dataset = dataset.rename_column(args.content_column, "content")
+    dataset = dataset.remove_columns(set(dataset.column_names) - set(['document_id', 'content']))
+
+    # Generate embeddings
+    print(f"Generating embeddings for {len(dataset)} documents")
+    embedding = Embedding(model_name=args.model, tokens_per_chunk=args.chunk_size)
+    start = time.time()
+    dataset_with_embeddings = embedding.embed(dataset, build_faiss_index=False)
+    end = time.time()
+    print(f"Generated embeddings in {end - start} seconds")
+
+    print(f"Saving dataset with embeddings to {args.output_dir}")
+    Corpus.save(dataset_with_embeddings, f"{args.output_dir}/{args.dataset_name}_{args.model}_{args.chunk_size}.parquet")
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
+
